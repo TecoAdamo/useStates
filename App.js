804a34469309df775f3e7b70333a2866bf63ce58
Mateus Adamo <adamomateus478@gmail.com>
2023-10-05 02:58:52 -0300

    useStateHook

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [count, setCount] = useState(0);
+
+  const CountNumbers = () => {
+    setCount(count + 1)
+  }
+
+  const CleanNumber = () => {
+    setCount(0)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.viewBtn}>
+        <TouchableOpacity onPress={CountNumbers}>
+          <Text style={{ color: 'white', textAlign: 'center' }}>Press me {count}</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.viewBtnClean}>
+        <TouchableOpacity
+        onPress={CleanNumber}
+        >
+          <Text style={{ color: 'white', textAlign: 'center' }}>Press for to clean!</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +40,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  viewBtn: {
+    backgroundColor: 'black',
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    width: 150,
+    margin: 0,
+  },
+  viewBtnClean: {
+    backgroundColor: 'black',
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    width: 150,
+    margin: 0,
+    top: 10
+  }
 });
